1395fca2502ea18f7c1dc800bd50686e9d70b961
Lisa Morales <morales.lisaaa@gmail.com>
2023-01-30 19:04:50 -0800

    genre colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css'
+import Artist from './components/Artist'
+import Genre from './components/Genre'
 
 const App = () => {
   return <div></div>
---
+++ src/components/Genre.js
@@ -0,0 +1,58 @@
+const Genre = (props) => {
+  let colorVariable = ''
+
+  switch (props.genre) {
+    case 'Sculptor':
+      colorVariable = 'Lavender'
+      break
+    case 'Fine Artist':
+      colorVariable = 'LavenderBlush'
+      break
+    case 'Manga Artist':
+      colorVariable = 'LemonChiffon'
+      break
+    case '3D Artist':
+      colorVariable = 'LightBlue'
+      break
+    case 'Comic Book Artist':
+      colorVariable = 'LightSteelBlue'
+      break
+    case 'Surrealist Painter':
+      colorVariable = 'MistyRose'
+      break
+    case 'Illustrators':
+      colorVariable = 'Plum'
+      break
+    case 'Painter':
+      colorVariable = 'RosyBrown'
+      break
+    case 'Contemporary Artist':
+      colorVariable = 'CadetBlue'
+      break
+    case 'Graphic Artist':
+      colorVariable = 'DarkSlateBlue'
+      break
+    case 'Oil Painting Artist':
+      colorVariable = 'DarkSeaGreen'
+      break
+    case 'Street Artist':
+      colorVariable = 'DarkSlateGrey'
+      break
+    case 'Visual Artist':
+      colorVariable = 'HoneyDew'
+      break
+    default:
+      colorVariable = 'white'
+  }
+
+  return (
+    <div
+      style={{ backgroundColor: colorVariable, color: '#000000' }}
+      className="genre"
+    >
+      <span>{props.genre}</span>
+    </div>
+  )
+}
+
+export default Genre
