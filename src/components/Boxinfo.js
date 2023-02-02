7eea05c2f389ce6a4fb463d4f5c365a6c9651fda
Lisa Morales <morales.lisaaa@gmail.com>
2023-02-01 16:09:02 -0800

    font update

--- src/components/Boxinfo.js
+++ src/components/Boxinfo.js
@@ -3,10 +3,14 @@ import React from 'react'
 const Boxinfo = (props) => {
   return (
     <center>
-      <h3>{props.name}</h3>
-      <p>{props.city}</p>
-      <p>{props.socials}</p>
-      <p>{props.website}</p>
+      <div className="name">
+        <h3>{props.name}</h3>
+        <div className="city">
+          <p>{props.city}</p>
+          <p>{props.socials}</p>
+          <p>{props.website}</p>
+        </div>
+      </div>
     </center>
   )
 }
