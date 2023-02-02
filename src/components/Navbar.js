c9a135e87b45e844e719b8773fd87f2a7aae8b11
Lisa Morales <morales.lisaaa@gmail.com>
2023-02-01 19:08:04 -0800

    ham updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,9 @@ const Navbar = () => {
     <div className="nav-bar home">
       <div class="menu">
         <div class="hamburger"></div>
-        <div class="hamburger"></div>
-        <div class="hamburger"></div>
+
         <div class="hamburgertwo"></div>
-        <div class="hamburgertwo"></div>
-        <div class="hamburgertwo"></div>
-        <div class="hamburgerthree"></div>
-        <div class="hamburgerthree"></div>
+
         <div class="hamburgerthree"></div>
         <div className="nav-info">
           <a>HOME</a>
